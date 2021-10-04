6fa3da4f09983f7b46c3910ec036553a1c685acd
Vinskky <naitsabesdv@gmail.com>
2021-10-04 10:56:21 +0200

    TDP/UDP init
    - Added new unity scene to use TDP/UDP
    - New base script for TDP/UDP excersice

---
+++ Xarxes_Projects/Assets/Scripts/UDP_TDP.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UDP_TDP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
