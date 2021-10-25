943a1e10ffdf501503b0b987a05f9191fb829017
M1R4B3L <amirabelmartinez@gmail.com>
2021-10-25 10:27:19 +0200

    Jordi is flying I hate him

---
+++ Lab3_Chat/Assets/Scripts/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+    Socket socket = null;
+
+    IPEndPoint ip = null;
+
+    int port = 6969;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Lab3_Chat/Assets/Scripts/Server.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+public class Server : MonoBehaviour
+{
+    Socket socketServer = null;
+
+    //Sockets Listening (Despues cambiar por lista de users)
+    ArrayList listenList;
+    //Sockets Writing
+    ArrayList writeList;
+    //Sockets Error
+    ArrayList errorList;
+
+    ArrayList acceptedClients;
+
+    IPEndPoint ip = null;
+
+    int serverPort = 6969;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        listenList = new ArrayList();
+        writeList = new ArrayList();
+        errorList = new ArrayList();
+
+        socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        ip = new IPEndPoint(IPAddress.Any, serverPort);
+        socketServer.Bind(ip);
+
+        Debug.Log("Server: Connected to port " + ip.Port + "with addres " + ip.Address);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        listenList.Add(socketServer);
+        while(socketServer.Poll(1000, SelectMode.SelectRead))
+        {
+            acceptedClients.Add(socketServer.Accept());
+        }
+    }
+}
