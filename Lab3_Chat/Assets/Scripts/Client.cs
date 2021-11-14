483b561470cc56c4611eb336687fd8a11fb411e5
Vinskky <naitsabesdv@gmail.com>
2021-11-14 20:08:55 +0100

    Begining of commands
    now user can use / command to do specific stuff. Needs work on it

--- Lab3_Chat/Assets/Scripts/Client.cs
+++ Lab3_Chat/Assets/Scripts/Client.cs
@@ -64,7 +64,66 @@ public class Client : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Return) && isConnected == false )
+        if (Input.GetKeyDown(KeyCode.Return) && chatField.text.StartsWith("/") && chatField.text != null)
+        {
+            //Read which command is
+            string[] data = chatField.text.Split(' ');
+
+            string command = data[0];
+            string userToSend = "";
+            string mssg = "";
+
+            if (command != "/whisper" && command != "/kick")
+            {
+                //only used for changeName, so we put _ on " ".
+                for (uint i = 1; i < data.Length; ++i)
+                {
+                    mssg += data[i].ToString() + "_";
+                }
+            }
+            else
+            {
+                userToSend = data[1].ToString();
+                for (uint i = 2; i < data.Length; ++i)
+                {
+                    mssg += data[i].ToString() + " ";
+                }
+            }
+
+            switch (command)
+            {
+                case "/help":
+                    {
+                        //Returns to user a message explaining all the commands.
+                        CommandHelp();
+                    }
+                    break;
+                case "/list":
+                    {
+                        //Returns to user a message with the list of active users.
+                        CommandList();
+                    }
+                    break;
+                case "/kick":
+                    {
+                        //User input name and if exist kicks him from the chat
+                        CommandKick(userToSend);
+                    }
+                    break;
+                case "/whisper":
+                    {
+                        CommandWhisper(userToSend, mssg);
+                    }
+                    break;
+                case "/changeName":
+                    {
+                        CommandChangeName(mssg);
+                    }
+                    break;
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) && isConnected == false )
         {
             Message sendMsg = new Message();
             sendMsg.clientName = loginField.text;
@@ -79,7 +138,7 @@ public class Client : MonoBehaviour
 
         if (isConnected == true)
         {
-            if (Input.GetKeyDown(KeyCode.Return) && chatField.text != "" && chatField.text != null)
+            if (Input.GetKeyDown(KeyCode.Return) && chatField.text != "" && chatField.text != null && chatField.text.StartsWith("/") == false)
             {
                 Message chatMsg = new Message();
                 chatMsg.type = Message.MessageType.Brodcast;
@@ -189,5 +248,37 @@ public class Client : MonoBehaviour
         }
     }
 
-        
+    void CommandHelp()
+    {
+        //returns message with all conditions about 
+        string helpString = "Welcome to this chatroom. You can use the next commands: /n" +
+                            "/help: display information about all available commands. /n" +
+                            "/list: display a list with all active users connected to the server. /n" +
+                            "/kick + ' ' + username: Disconnects user from the server. /n" +
+                            "/whisper + ' ' + username + ' ' + message: Sends exclusively a message to the selected user. /n" + 
+                            "changeName + ' ' + newName: change you name";
+
+    }
+    void CommandList()
+    {
+
+    }
+
+    void CommandKick(string user)
+    {
+        //check if user exist
+        //desconect user
+    }
+    void CommandWhisper(string user, string msg)
+    {
+        //check if user exist
+        //send to that user message
+    }
+
+    void CommandChangeName(string newName)
+    {
+        //modify class Message clientName to newName
+        //update list
+        //send message to all users saying now clientname is newName?
+    }
 }
